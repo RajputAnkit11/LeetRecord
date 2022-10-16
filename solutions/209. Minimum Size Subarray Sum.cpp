345635481f5de038403fe60956aecd7416de51e2
Ankit Singh <56732288+RajputAnkit11@users.noreply.github.com>
2022-10-16 22:28:23 +0530

    89 ms

--- solutions/209. Minimum Size Subarray Sum.cpp
+++ solutions/209. Minimum Size Subarray Sum.cpp
@@ -1,19 +1,33 @@
-                cout<<sum<<endl;
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        
+        int n = nums.size();
+        int l = 0, r = 0;
+        long long int sum = 0;
+        int ans = INT_MAX;
+         
+        while(true){
+            bool f1 = false;
+            bool f2 = false;
+            while(r<n){
+                f1 = true;
+                sum += nums[r];
+                r++;
                 if(sum>=target){
                     break;
                 }
                 f1 = false;
             }
-            cout<<"here\n";
+            
             while(l<r){
                 if(f1 == false) break;
                 f2 = true;
                 sum -= nums[l];
-                cout<<"sum 2 = "<<sum<<endl;
+                
                 l++;
                 if(sum<target){
                     ans = min(ans,r-l+1);
-                    cout<<"ans "<<ans<<endl;
                     break;
                 }
                 else{
