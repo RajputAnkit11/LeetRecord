f73fd1a21909c356937d7ba9e62f37dd1e052180
Ankit Singh <56732288+RajputAnkit11@users.noreply.github.com>
2022-10-29 15:45:34 +0530

    11 ms

--- solutions/122. Best Time to Buy and Sell Stock II.cpp
+++ solutions/122. Best Time to Buy and Sell Stock II.cpp
@@ -24,10 +24,30 @@ public:
     }
     
     int maxProfit(vector<int>& prices) {
+        // int n = prices.size();
+        // vector<vector<int>> dp(n,vector<int>(2,-1));
+        // // 0 means cant buy, 1 means can buy 
+        // int canBuy = 1; 
+        // return helper(0,canBuy,prices,dp);
+        
         int n = prices.size();
-        vector<vector<int>> dp(n,vector<int>(2,-1));
-        // 0 means cant buy, 1 means can buy 
-        int canBuy = 1; 
-        return helper(0,canBuy,prices,dp);
+        vector<vector<int>> dp(n+1,vector<int>(2,-1));
+        dp[n][0] = dp[n][1] = 0;
+        
+        for(int i=n-1;i>=0;i--){
+            for(int canBuy=0;canBuy<=1;canBuy++){
+                int profit = INT_MIN;
+                if(canBuy){
+                    profit = max(-prices[i]+dp[i+1][0], dp[i+1][1]);
+                }
+                else{
+                    profit = max(prices[i] + dp[i+1][1], dp[i+1][0]);
+                }
+                dp[i][canBuy] = profit;
+            }
+        }
+        
+        return dp[0][1];
     }
+    
 };
