2b896573598074ba6ef2790a518540c31b5c710a
Ankit Singh <56732288+RajputAnkit11@users.noreply.github.com>
2022-11-01 17:29:04 +0530

    875 ms

---
+++ solutions/2454. Next Greater Element IV.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> secondGreaterElement(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> greater(n,n),ans(n);
+        stack<int> st;
+        
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty()&&nums[st.top()]<=nums[i]){
+                st.pop();
+            }
+            if(!st.empty()){
+                greater[i]=st.top();
+            }
+            
+            st.push(i);
+        }
+        for(auto i: greater){
+            cout<<i<<" ";
+        }
+        for(int i=0;i<n;i++){
+            int l=greater[i]+1;
+            
+            while(l<n && nums[i]>=nums[l]){
+                l=greater[l];
+            }
+            if(l>=n){
+              ans[i]=-1;
+            }
+            else {
+              ans[i]=nums[l];
+            }
+        }
+        
+        return ans;
+    }
+};
